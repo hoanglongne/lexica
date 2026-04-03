d2778a37fb2db15f3860bcc5700e6e4a6a4bb089
hoanglongne <73406617+hoanglongne@users.noreply.github.com>
2026-04-04 00:06:51 +0700

    PWA setup

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,15 @@
 import type { NextConfig } from "next";
 
+// @ts-ignore - next-pwa types are outdated for Next.js 16
+import withPWA from "next-pwa";
+
 const nextConfig: NextConfig = {
-  /* config options here */
+  turbopack: {}, // Allow webpack config from next-pwa
 };
 
-export default nextConfig;
+export default withPWA({
+  dest: "public",
+  register: true,
+  skipWaiting: true,
+  disable: process.env.NODE_ENV === "development",
+})(nextConfig as any) as NextConfig;
